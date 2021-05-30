51d929637e1969b0b939afd456c928ed1560d1ca
Karina <karinaimiafamilia@gmail.com>
2021-05-31 00:05:23 +0300

    Добавил файлы компонентов и стилизацию

---
+++ src/components/UsersList/UsersList.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import "./UsersList.css";
+
+const UsersList = () => {
+  return (
+    <div>
+      <h1>Employees</h1>
+      <div className="list">
+        <div className="UsersList">
+          <h2>A</h2>
+        </div>
+        <div className="UsersList">
+          <h2>B</h2>
+        </div>
+        <div className="UsersList">
+          <h2>C</h2>
+        </div>
+        <div className="UsersList">
+          <h2>D</h2>
+        </div>
+        <div className="UsersList">
+          <h2>E</h2>
+        </div>
+        <div className="UsersList">
+          <h2>F</h2>
+        </div>
+        <div className="UsersList">
+          <h2>G</h2>
+        </div>
+        <div className="UsersList">
+          <h2>H</h2>
+        </div>
+        <div className="UsersList">
+          <h2>I</h2>
+        </div>
+        <div className="UsersList">
+          <h2>J</h2>
+        </div>
+        <div className="UsersList">
+          <h2>K</h2>
+        </div>
+        <div className="UsersList">
+          <h2>L</h2>
+        </div>
+        <div className="UsersList">
+          <h2>M</h2>
+        </div>
+        <div className="UsersList">
+          <h2>N</h2>
+        </div>
+        <div className="UsersList">
+          <h2>O</h2>
+        </div>
+        <div className="UsersList">
+          <h2>P</h2>
+        </div>
+        <div className="UsersList">
+          <h2>Q</h2>
+        </div>
+        <div className="UsersList">
+          <h2>R</h2>
+        </div>
+        <div className="UsersList">
+          <h2>S</h2>
+        </div>
+        <div className="UsersList">
+          <h2>T</h2>
+        </div>
+        <div className="UsersList">
+          <h2>U</h2>
+        </div>
+        <div className="UsersList">
+          <h2>V</h2>
+        </div>
+        <div className="UsersList">
+          <h2>X</h2>
+        </div>
+        <div className="UsersList">
+          <h2>Y</h2>
+        </div>
+        <div className="UsersList">
+          <h2>Z</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UsersList;
